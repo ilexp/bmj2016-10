f2d4aa7ffdaac9bc5f66cd7998c0c412baf606ae
Adam <adam@adamslair.net>
2016-10-22 12:17:50 +0200

    Initial Setup

---
+++ Game/Source/Code/CorePlugin/YourCustomComponentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+
+namespace Game
+{
+	public class YourCustomComponentType : Component
+	{
+		
+	}
+}
---
+++ Game/Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace Game.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class GameEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "GameEditorPlugin"; }
+		}
+	}
+}
